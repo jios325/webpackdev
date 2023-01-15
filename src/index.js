3acce9be1cf432a19ac528278c10c85e0fe906c0
Juan carlos salgado <jios@MacBook-Air-de-Juan.local>
2023-01-14 23:26:06 -0500

    cambios fianles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/test.sass';
+
 
 (async function App() {
   const main = null || document.getElementById('main');
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,11 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 
 module.exports = {
     entry: './src/index.js',
@@ -15,6 +11,8 @@ module.exports = {
         filename: '[name][contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
+    mode: 'development',
+    watch: true,
     resolve:{
         extensions:['.js'],
         alias:{
@@ -81,13 +79,6 @@ module.exports = {
          }),
         
          new Dotenv(),
-            new CleanWebpackPlugin(),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ],
-    }
+
+    ]
 }
